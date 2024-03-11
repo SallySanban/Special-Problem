c0cbaecb4efb9e8ba210b0010817d73eeb24ec37
SallySanban <johanna.eikou@gmail.com>
2024-03-11 19:24:54 +0800

    fix: fixed playerName if no input is given

--- Assets/Resources/Scripts/Player.cs
+++ Assets/Resources/Scripts/Player.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public static string playerName = "Player";
+    public static string playerName;
     public static int choicesIncorrect = 5; //everyone starts with 5 (total number of choices)
 
     public void readInputField(string input)
     {
-        playerName = input;
+        if(input == "")
+        {
+            playerName = "Player";
+        }
+        else
+        {
+            playerName = input;
+        }
 
         Debug.Log(playerName);
     }
